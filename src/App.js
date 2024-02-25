d95bbb893c0ec890ab029aaf477a3215679a8ec3
Andee-R <aqueen.reid@gmail.com>
2024-02-25 13:39:53 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,19 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <Search />
+        <footer>
+          Open-source code on,{" "}
+          <a
+            href="https://github.com/Andee-R/react-weather-app"
+            target="_blank"
+            rel="noreferrer">
+            Github
+          </a>{" "}
+          hosted on{" "}
+          <a href="" target="_blank" rel="noreferrer">
+            Netlify
+          </a>{" "}
+        </footer>
       </header>
     </div>
   );
