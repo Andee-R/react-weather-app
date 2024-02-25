58410779c1d13421bcb0730df530a5aa99fb20ec
Andee-R <aqueen.reid@gmail.com>
2024-02-25 13:16:40 -0700

    created react app

---
+++ src/Search.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Search() {
+  return (
+    <form>
+      <input type="Search" placeholder="Search City..." />
+      <button type="Submit"> Search </button>
+    </form>
+  );
+}
